f9d292c5a7d519882f5f18c131afe05910ecc7f9
Sosthène Guédon <sosthene.gued@gmail.com>
2020-05-07 13:55:35 +0200

    Fix visibility of api errors

--- crates/peertube-api/src/lib.rs
+++ crates/peertube-api/src/lib.rs
@@ -4,10 +4,9 @@ extern crate reqwest;
 extern crate serde_json;
 extern crate tokio;
 
-mod error;
+pub mod error;
 mod instance;
 mod video;
 
-pub use error::*;
 pub use instance::Instance;
 pub use video::{Resolution, Video};
