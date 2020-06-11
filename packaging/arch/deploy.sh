eb9518b0e72fa3a79cc96b24b4e464fefcaaf3db
Sosthène Guédon <sosthene.gued@gmail.com>
2020-06-11 23:15:32 +0200

    Add auto-deploy

---
+++ packaging/arch/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+release=$(git describe --tags|sed 's/^v//')
+
+ssh-agent
+ssh-add "$AUR_DEPLOY_KEY"
+git clone 'ssh://aur@aur.archlinux.org/peertube-viewer-rs.git'
+cd peertube-viewer-rs
+sed "s/^pkgver=$/pkgver=$release/" ../raw_PKGBUILD > PKGBUILD
+sed -i "s/^#md5sums=$/$(makepkg -g)/" PKGBUILD
+makepkg --printsrcinfo > .SRCINFO
+git add PKGBUILD
+git add .SRCINFO
+git commit -m "Release $release"
