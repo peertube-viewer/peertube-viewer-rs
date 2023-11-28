f8763459f8a9f0bbaba6d295d014dff1be2d0456
Sosthène Guédon <sosthene@guedon.gdn>
2023-11-28 21:49:34 +0100

    Fix release script

--- scripts/create-realease-tarbal.sh
+++ scripts/create-realease-tarbal.sh
@@ -2,4 +2,4 @@
 
 cp target/release/peertube-viewer-rs peertube-viewer-rs
 mkdir linux-build
-tar -czf linux-build/peertube-viewer-rs-"$(git describe --tags)".tar.gz completions/* peertube-viewer-rs peertube-viewer-rs.1 LICENSE
+tar --transform 'flags=r;s|COPYING.md|LICENSE|' -czf linux-build/peertube-viewer-rs-"$(git describe --tags)".tar.gz completions/* peertube-viewer-rs peertube-viewer-rs.1 COPYING.md
